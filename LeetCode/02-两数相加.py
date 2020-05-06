d4dcaaf69897c557c6b359020e0d39086e9aea50
ZWT <wentao.zhang@broadlink.com.cn>
2020-05-06 12:56:54 +0800

    add 02

---
+++ LeetCode/02-两数相加.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+
+        return int_result
+
+
+if __name__ == '__main__':
+    S = Solution()
+    result = S.addTwoNumbers([2, 4, 3], [5, 6, 4])
+    print(result)
