d21749f697695d3a0c1dbad7dd9b4769c84111c5
ZWT <wentao.zhang@broadlink.com.cn>
2020-05-06 10:50:26 +0800

    modify 01

--- LeetCode/01-两数之和.py
+++ LeetCode/01-两数之和.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
             lens = len(nums)
             j = -1
 
@@ -8,9 +8,16 @@ class Solution:
                     if (nums.count(target - nums[i]) == 1)&(target - nums[i] == nums[i]):   # 如果nums[i]仅出现一次，且nums[i]=nums[i]，说明找到的是nums[i]本身
                         continue
                     else:
-                        j = nums.index(target - nums[i], i + 1) # 从i+1开始找
+                        j = nums.index(target - nums[i], i + 1)  # 从i+1开始找
                         break
             if j > 0:
                 return [i, j]
             else:
                 return 'not found'
+
+if __name__ == '__main__':
+    s = Solution()
+    tuple1 = [2, 5, 11, 7, 2]
+    target1 = 18
+    result = s.twoSum(tuple1, target1)
+    print(result)
